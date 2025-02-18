55936ff0cf0158160cc0ebdd948b971ea8976a61
bailee1531 <baileesegars2016@gmail.com>
2025-02-18 17:45:00 -0600

    Transaction Logging (#29)
    * Added csv file for logging. Added script to generate a unique transaction ID. Implemented script into deposit and withdrawal scripts. Deleted old savings account script. Modified account creation to make sure account ID is unique
    
    * Update scripts/makeDeposit.py
    
    Co-authored-by: Sierra <66871263+hani-sierra@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Sierra <66871263+hani-sierra@users.noreply.github.com>

--- scripts/withdrawMoney.py
+++ scripts/withdrawMoney.py
@@ -1,6 +1,7 @@
 # Bailee Segars
-import pandas as pd
+from transactionLog import generate_transaction_ID
 from decimal import Decimal
+import pandas as pd
 import os
 
 def withdraw(accID, amount) -> dict:
@@ -30,15 +31,18 @@ def withdraw(accID, amount) -> dict:
     # Creates dataframe with csv data
     # Gets row for requested customer
     accPath = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../csvFiles/accounts.csv'))
+    logPath = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../csvFiles/logs.csv'))
     accInfo = pd.read_csv(accPath)
+    logInfo = pd.read_csv(logPath)
 
     if accID not in accInfo['AccountID'].values:
         return {"status": "error", "message": f"Source account {accID} not found."}
 
-    custIndex = accInfo.loc[accInfo['AccountID'] == accID].index[0]
-    accType = accInfo.at[custIndex, 'AccountType']
+    accIndex = accInfo.loc[accInfo['AccountID'] == accID].index[0]
+    accType = accInfo.at[accIndex, 'AccountType']
+    custID = accInfo.at[accIndex, 'CustomerID']
     accInfo['CurrBal'] = accInfo['CurrBal'].apply(lambda x: Decimal(str(x)).quantize(Decimal('0.00')))
-    currentBal = Decimal(accInfo.at[custIndex, 'CurrBal'])
+    currentBal = Decimal(accInfo.at[accIndex, 'CurrBal'])
 
     # Account type and balance validation
     if accType != 'Checking' and accType != 'Savings':
@@ -48,7 +52,12 @@ def withdraw(accID, amount) -> dict:
 
     # Completes withdrawal and updates csv file
     currentBal -= Decimal(amount).quantize(Decimal('0.00'))
-    accInfo.at[custIndex, 'CurrBal'] = Decimal(currentBal).quantize(Decimal('0.00'))
+    accInfo.at[accIndex, 'CurrBal'] = Decimal(currentBal).quantize(Decimal('0.00'))
     accInfo.to_csv(accPath, index=False)
+
+    transactionID = generate_transaction_ID(logInfo)
+    newLog = {'AccountID': accID, 'CustomerID': custID, 'TransactionType': 'Withdrawal', 'Amount': amount,'TransactionID': transactionID}
+    logInfo.loc[len(accInfo)] = newLog
+    logInfo.to_csv(logPath, index=False)
 
     return {"status": "success", "message": f"Withdrew {amount} from account {accID}."}
