9767687ce8b28c18273982dc73b0298c542dc109
bailee1531 <baileesegars2016@gmail.com>
2025-03-02 18:49:10 -0600

    Created unittest file for a deposit

---
+++ unitTests/depositTest.py
@@ -0,0 +1,34 @@
+from makDeposit import deposit
+from decimal import Decimal
+import unittest
+
+class TestDeposit(unittest.TestCase):
+    def test_param_type(self):
+        def params_test(accID, amount):
+            self.assertIsInstance(accID, int)
+            self.assertIsInstance(amount, Decimal)
+        
+        params_test(615, 167.89)
+        params_test(789, 20)
+
+    def test_deposit(self):
+        self.assertTrue(deposit(615, 167.89))
+        self.assertTrue(deposit(789, 20))
+
+    def test_invalid_accID(self):
+        with self.assertRaises(TypeError) as context:
+            deposit('test', 167.89)
+        self.assertEqual('accID must be an integer', str(context.exception))
+
+    def test_invalid_amount(self):
+        with self.assertRaises(TypeError) as context:
+            deposit(615, 'test')
+        self.assertEqual('amount must be a decimal', str(context.exception))
+
+    def test_invalid_amount_list(self):
+        with self.assertRaises(TypeError) as context:
+            deposit(615, [20,30,40])
+        self.assertEqual('amount must be a decimal', str(context.exception))
+
+if __name__ == '__main__':
+    unittest.main()
